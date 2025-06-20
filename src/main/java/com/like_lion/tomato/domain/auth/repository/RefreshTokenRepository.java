100ba625381c4986ce1104fe80c65690f93d4bc3
KKT <kimgt2828@gmail.com>
2025-06-20 22:12:41 +0900

    Hotfix : Resolve build errors related to auth
    - Delete any kinds of codes that finding RefreshToken by User Id
    
    - Fix query error of searching member page b par and year and role in

--- src/main/java/com/like_lion/tomato/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/like_lion/tomato/domain/auth/repository/RefreshTokenRepository.java
@@ -11,7 +11,4 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Stri
     void deleteAllByUsername(String username);
 
     Optional<RefreshToken> findByUsername(String username);
-
-
-    Optional<RefreshToken> findByUserId(String userId);
 }
