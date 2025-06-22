1654f3d44b7675e1ce49c6382b2cc84d44473bbc
KKT <kimgt2828@gmail.com>
2025-06-22 18:18:19 +0900

    Feat(#2): Member 엔티티 수정

--- src/main/java/com/like_lion/tomato/domain/member/entity/Member.java
+++ src/main/java/com/like_lion/tomato/domain/member/entity/Member.java
@@ -1,24 +1,38 @@
 package com.like_lion.tomato.domain.member.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.sql.Array;
 import java.util.ArrayList;
 
+@Getter
+
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true, length = 20)
     private String username;
+
+    @Column(nullable = false, unique = true, length = 30)
     private String email;
+
     private String profileImageUrl;
-    private String phone;
-    private String birth;
-    private String role;
+
+    // @Column(EnumType.STRING)
+    // private Role role; // ROLE만든 후 구현!
+    @Column(nullable = false, length = 30)
     private String part;
+
+    @Column(nullable = false, length = 200)
     private String introduce;
+
     // 구글 로그인만 진행하므로 provider, provider_id 필드는 생략, 만약 네이버, 카카오 로그인 확장시 추가 구현
 
     // 과제 구현 후 양방향 매핑 예정
