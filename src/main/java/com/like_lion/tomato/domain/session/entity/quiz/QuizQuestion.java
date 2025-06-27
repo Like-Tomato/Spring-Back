f6ccf65b50a7e4967a8ea7763e35570ca19d8954
KKT <kimgt2828@gmail.com>
2025-06-28 02:57:51 +0900

    Feat(#18): 세션 퀴즈 도메인 추가 - QuizOption, QuizQuestion, SessionQuiz 엔티티 추가 - 연결관계 설정   - 세션, 문제를 양방향으로 처리(바로 개수 조회)   - 문제, 옵션을 단방향 처리 후 즉시 조회 적용

---
+++ src/main/java/com/like_lion/tomato/domain/session/entity/quiz/QuizQuestion.java
@@ -0,0 +1,30 @@
+package com.like_lion.tomato.domain.session.entity.quiz;
+import com.like_lion.tomato.global.id.DomainId;
+import com.like_lion.tomato.global.id.DomainType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class QuizQuestion {
+
+    @DomainId(DomainType.QUIZ_QUESTION)
+    @Id
+    @Column(name = "quiz_question_id")
+    private String id;
+
+    @Column(nullable = false, length = 200)
+    private String question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "session_quiz")
+    private SessionQuiz sessionQuiz;
+
+    public void setSessionQuiz(SessionQuiz quizQuestion) {
+        this.sessionQuiz = sessionQuiz;
+    }
+
+}
