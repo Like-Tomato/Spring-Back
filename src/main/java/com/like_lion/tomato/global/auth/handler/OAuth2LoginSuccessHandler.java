2c29ba592fa95dd03c659e5f157fd453dcf28b5f
KKT <kimgt2828@gmail.com>
2025-06-28 17:40:34 +0900

    Feat(#8-jwt): JWT 토큰 발급 로직 구현 - JWT 토큰 발급을 위한 JwtTockenProvider 클래스 추가 - TockenDto 클래스 추가 - OAuth2 로그인 성공 시 JWT 토큰을 발급하는 OAuth2LoginSuccessHandler 구현 예정

---
+++ src/main/java/com/like_lion/tomato/global/auth/handler/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,7 @@
+package com.like_lion.tomato.global.auth.handler;
+
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+}
