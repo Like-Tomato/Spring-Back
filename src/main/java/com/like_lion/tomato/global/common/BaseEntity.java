e2775cd2ccfdae1e8a71100c3d134ac8c8e6812a
saeyeonn <sy01lim@naver.com>
2025-07-01 16:08:47 +0900

    refactor: 오탈자 수정 (BaseEntitiy -> BaseEntity)

---
+++ src/main/java/com/like_lion/tomato/global/common/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.like_lion.tomato.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
