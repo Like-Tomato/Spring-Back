f6fc80a977faec8edd762eec44bf7b6b7e11c0a7
saeyeonn <sy01lim@naver.com>
2025-07-02 17:42:42 +0900

    Chore(#23-project): 패키지 구조 변경 (archive 내 project/gallery 분리)

--- src/main/java/com/like_lion/tomato/domain/archive/exception/ArchiveErrorCode.java
+++ src/main/java/com/like_lion/tomato/domain/archive/exception/ArchiveErrorCode.java
@@ -1,4 +1,4 @@
-package com.like_lion.tomato.domain.archieve.exception;
+package com.like_lion.tomato.domain.archive.exception;
 
 import com.like_lion.tomato.global.exception.ErrorCode;
 import lombok.Getter;
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 public enum ArchiveErrorCode implements ErrorCode {
     // 잘못된 요청 오류
     INVALID_CATEGORY(400,  "유효하지 않은 카테고리입니다"),
+    INVALID_DATE_RANGE(400,  "유효하지 않은 날짜입니다."),
     INVALID_SORT_OPTION(400,  "유효하지 않은 정렬 옵션입니다"),
     INVALID_STATUS(400, "유효하지 않은 상태값입니다"),
     TOO_MANY_IMAGES(400,  "이미지는 최대 10개까지 업로드 가능합니다"),
