42c62c06daedc55c1a2b94c93fce5a17df9bb0fb
saeyeonn <sy01lim@naver.com>
2025-07-02 21:30:34 +0900

    Initial commit: Clean main

---
+++ src/main/java/com/like_lion/tomato/ApiTestController.java
@@ -0,0 +1,12 @@
+package com.like_lion.tomato;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiTestController {
+    @GetMapping("/")
+    public String test() {
+        return "test infra";
+    }
+}
