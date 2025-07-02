312080a7b2854f90cfb37604c6f04d149adb7979
saeyeonn <sy01lim@naver.com>
2025-07-02 11:37:24 +0900

    Fix(#13-recruitment): ApplicantResponse는 지원자의 간단한 정보만 가지도록 변경
    - 레코드 내부에 Detail 추가 선언하여 세부 정보를 갖도록 수정

--- src/main/java/com/like_lion/tomato/domain/recruitment/dto/applicant/ApplicantResponse.java
+++ src/main/java/com/like_lion/tomato/domain/recruitment/dto/applicant/ApplicantResponse.java
@@ -1,79 +1,98 @@
 package com.like_lion.tomato.domain.recruitment.dto.applicant;
 
-import com.like_lion.tomato.domain.recruitment.dto.application.ApplicationResponse;
 import com.like_lion.tomato.domain.recruitment.dto.question.QuestionAnswerDto;
 import com.like_lion.tomato.domain.recruitment.entity.application.Application;
-import com.like_lion.tomato.domain.recruitment.service.application.ApplicantService;
+import com.like_lion.tomato.domain.recruitment.entity.constant.ApplicationStatus;
 import com.like_lion.tomato.global.common.enums.Part;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.function.Function;
 
 @Builder
 public record ApplicantResponse(
         String id,
         String name,
-        String major,
-        String studentId,
-        String email,
-        String phone,
-        List<QuestionAnswerDto> commonAnswers,
-        List<QuestionAnswerDto> partAnswers,
-        String portfolioUrls,
-        Part part,
+        ApplicationStatus status,
         LocalDateTime appliedAt,
         String reviewerId
 ) {
-    @Builder
+
     public static ApplicantResponse from(Application application, String reviewerId) {
-        List<QuestionAnswerDto> commonAnswers = application.getCommonAnswers().stream()
-                .map(QuestionAnswerDto::fromCommon)
-                .toList();
-
-        List<QuestionAnswerDto> partAnswers = application.getPartAnswers().stream()
-                .map(QuestionAnswerDto::fromPart)
-                .toList();
-
-        return ApplicantResponse.builder()
-                .id(application.getId())
-                .name(application.getUsername())
-                .major(application.getMajor())
-                .studentId(application.getStudentId())
-                .email(application.getMember().getEmail())
-                .phone(application.getPhone())
-                .commonAnswers(commonAnswers)
-                .partAnswers(partAnswers)
-                .portfolioUrls(application.getPortfolioUrl())
-                .part(application.getPart())
-                .appliedAt(application.getSubmittedAt())
-                .reviewerId(reviewerId)
-                .build();
+        return new ApplicantResponse(
+                application.getId(),
+                application.getUsername(),
+                application.getStatus(),
+                application.getSubmittedAt(),
+                reviewerId
+        );
     }
 
-    public static ApplicantResponse fromPart(Application application, Part part, String reviewerId) {
-        List<QuestionAnswerDto> commonAnswers = application.getCommonAnswers().stream()
-                .map(QuestionAnswerDto::fromCommon)
-                .toList();
+    @Builder
+    public record Detail(
+            String id,
+            String name,
+            String major,
+            String studentId,
+            String email,
+            String phone,
+            List<QuestionAnswerDto> commonAnswers,
+            List<QuestionAnswerDto> partAnswers,
+            String portfolioUrls,
+            Part part,
+            LocalDateTime appliedAt,
+            String reviewerId
+    ) {
+        public static Detail from(Application application, String reviewerId) {
+            List<QuestionAnswerDto> commonAnswers = application.getCommonAnswers().stream()
+                    .map(QuestionAnswerDto::fromCommon)
+                    .toList();
 
-        List<QuestionAnswerDto> partAnswers = application.getPartAnswers().stream()
-                .map(QuestionAnswerDto::fromPart)
-                .toList();
+            List<QuestionAnswerDto> partAnswers = application.getPartAnswers().stream()
+                    .map(QuestionAnswerDto::fromPart)
+                    .toList();
 
-        return ApplicantResponse.builder()
-                .id(application.getId())
-                .name(application.getUsername())
-                .major(application.getMajor())
-                .studentId(application.getStudentId())
-                .email(application.getMember().getEmail())
-                .phone(application.getPhone())
-                .commonAnswers(commonAnswers)
-                .partAnswers(partAnswers)
-                .portfolioUrls(application.getPortfolioUrl())
-                .part(part)
-                .appliedAt(application.getSubmittedAt())
-                .reviewerId(reviewerId)
-                .build();
+            return Detail.builder()
+                    .id(application.getId())
+                    .name(application.getUsername())
+                    .major(application.getMajor())
+                    .studentId(application.getStudentId())
+                    .email(application.getMember().getEmail())
+                    .phone(application.getPhone())
+                    .commonAnswers(commonAnswers)
+                    .partAnswers(partAnswers)
+                    .portfolioUrls(application.getPortfolioUrl())
+                    .part(application.getPart())
+                    .appliedAt(application.getSubmittedAt())
+                    .reviewerId(reviewerId)
+                    .build();
+        }
+
+        public static Detail fromPart(Application application, Part part, String reviewerId) {
+            List<QuestionAnswerDto> commonAnswers = application.getCommonAnswers().stream()
+                    .map(QuestionAnswerDto::fromCommon)
+                    .toList();
+
+            List<QuestionAnswerDto> partAnswers = application.getPartAnswers().stream()
+                    .map(QuestionAnswerDto::fromPart)
+                    .toList();
+
+            return Detail.builder()
+                    .id(application.getId())
+                    .name(application.getUsername())
+                    .major(application.getMajor())
+                    .studentId(application.getStudentId())
+                    .email(application.getMember().getEmail())
+                    .phone(application.getPhone())
+                    .commonAnswers(commonAnswers)
+                    .partAnswers(partAnswers)
+                    .portfolioUrls(application.getPortfolioUrl())
+                    .part(part)
+                    .appliedAt(application.getSubmittedAt())
+                    .reviewerId(reviewerId)
+                    .build();
+        }
     }
+
+
 }
