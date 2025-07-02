65c5f091e1ad7f399c328f76aa5d276d294d632a
saeyeonn <sy01lim@naver.com>
2025-07-02 13:27:30 +0900

    Feat(#22-admin-recruitment): 질문 업로드하는 컨트롤러 메소드 정의

--- src/main/java/com/like_lion/tomato/domain/recruitment/api/QuestionController.java
+++ src/main/java/com/like_lion/tomato/domain/recruitment/api/QuestionController.java
@@ -1,26 +1,35 @@
 package com.like_lion.tomato.domain.recruitment.api;
 
-import com.like_lion.tomato.domain.recruitment.dto.application.ApplicationRequest;
-import com.like_lion.tomato.domain.recruitment.dto.application.ApplicationResponse;
+import com.like_lion.tomato.domain.recruitment.dto.question.QuestionInfo;
+import com.like_lion.tomato.domain.recruitment.dto.question.QuestionUploadRequest;
 import com.like_lion.tomato.domain.recruitment.dto.question.QuestionResponse;
-import com.like_lion.tomato.domain.recruitment.service.application.ApplicationService;
-import com.like_lion.tomato.domain.recruitment.service.application.RecruitmentQuestionService;
+import com.like_lion.tomato.domain.recruitment.service.application.QuestionService;
 import com.like_lion.tomato.global.common.enums.Part;
 import com.like_lion.tomato.global.exception.response.ApiResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/recruit")
+@RequestMapping("/api/v1/recruit/questions")
 @RequiredArgsConstructor
 public class QuestionController {
-    private final RecruitmentQuestionService questionService;
-    private final ApplicationService applicationService;
+    private final QuestionService questionService;
 
-    @GetMapping("/questions/{part}")
-    public ApiResponse<QuestionResponse> getQuestionsByPart(@PathVariable Part part) {
-        QuestionResponse response = questionService.getQuestionsByPart(part);
+    @PostMapping
+    public ApiResponse<QuestionResponse> createQuestions(
+            @RequestParam Part part,
+            @RequestBody QuestionUploadRequest requests,
+            @RequestHeader("Authorization") String authorization
+    ) {
+        QuestionResponse response = questionService.uploadQuestions(part, requests, authorization);
+        return ApiResponse.success(response);
+    }
+
+    @GetMapping("/{part}")
+    public ApiResponse<QuestionInfo> getQuestionsByPart(@PathVariable Part part) {
+        QuestionInfo response = questionService.getQuestionsByPart(part);
         return ApiResponse.success(response);
     }
 
