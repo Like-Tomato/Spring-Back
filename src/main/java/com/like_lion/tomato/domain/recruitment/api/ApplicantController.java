3ac70a776065298e5b4a16f39af1d5a0669cf074
saeyeonn <sy01lim@naver.com>
2025-07-02 15:08:30 +0900

    Chore(#22-admin-recruitment): 지원자 검토 컨트롤러와 합격 로직 컨트롤러 분리

--- src/main/java/com/like_lion/tomato/domain/recruitment/api/ApplicantController.java
+++ src/main/java/com/like_lion/tomato/domain/recruitment/api/ApplicantController.java
@@ -1,15 +1,10 @@
 package com.like_lion.tomato.domain.recruitment.api;
 
 import com.like_lion.tomato.domain.recruitment.dto.applicant.ApplicantResponse;
-import com.like_lion.tomato.domain.recruitment.dto.applicant.PassResponse;
 import com.like_lion.tomato.domain.recruitment.dto.applicant.StatusResponse;
-import com.like_lion.tomato.domain.recruitment.dto.application.ApplicationRequest;
-import com.like_lion.tomato.domain.recruitment.dto.application.ApplicationResponse;
 import com.like_lion.tomato.domain.recruitment.service.application.ApplicantService;
-import com.like_lion.tomato.domain.recruitment.service.application.ApplicationService;
 import com.like_lion.tomato.global.common.enums.Part;
 import com.like_lion.tomato.global.exception.response.ApiResponse;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -36,16 +31,6 @@ public class ApplicantController {
             @RequestHeader("Authorization") String authorization
     ) {
         StatusResponse response = applicantService.getApplicants(part, round, authorization);
-        return ApiResponse.success(response);
-    }
-
-    @PostMapping("/pass/{round}")
-    public ApiResponse<PassResponse> passApplicants(
-            @PathVariable int round,
-            @RequestParam Part part,
-            @RequestHeader("Authorization") String authorization
-    ) {
-        PassResponse response = applicantService.passApplicants(round, part, authorization);
         return ApiResponse.success(response);
     }
 }
