3b8c566da64deda705d5c84d70f895e56d649aba
saeyeonn <sy01lim@naver.com>
2025-07-03 13:42:54 +0900

    Feat(#23-project): Add jpa methods to find by year/category/both

--- src/main/java/com/like_lion/tomato/domain/archive/project/repository/ProjectRepository.java
+++ src/main/java/com/like_lion/tomato/domain/archive/project/repository/ProjectRepository.java
@@ -1,7 +1,22 @@
 package com.like_lion.tomato.domain.archive.project.repository;
 
 import com.like_lion.tomato.domain.archive.project.entity.Project;
+import com.like_lion.tomato.domain.archive.project.entity.constant.ProjectCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProjectRepository extends JpaRepository<Project, String> {
+    @Query("SELECT p FROM Project p JOIN p.generation g WHERE g.year = :year")
+    Page<Project> findByYear(@Param("year") Integer year, Pageable pageable);
+
+    Page<Project> findByCategory(ProjectCategory category, Pageable pageable);
+
+    @Query("SELECT p FROM Project p JOIN p.generation g WHERE g.year = :year AND p.category = :category")
+    Page<Project> findByYearAndCategory(@Param("year") Integer year,
+                                        @Param("category") ProjectCategory category,
+                                        Pageable pageable);
+
 }
