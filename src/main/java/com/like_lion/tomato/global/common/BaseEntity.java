29920233bafabbe69133895ff2e235620f71f571
saeyeonn <sy01lim@naver.com>
2025-07-03 13:16:04 +0900

    Hotfix: Fix @Persist error of BaseEntity

--- src/main/java/com/like_lion/tomato/global/common/BaseEntity.java
+++ src/main/java/com/like_lion/tomato/global/common/BaseEntity.java
@@ -1,8 +1,7 @@
 package com.like_lion.tomato.global.common;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import com.like_lion.tomato.global.id.IdGeneratorListener;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
@@ -13,14 +12,30 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners({AuditingEntityListener.class, IdGeneratorListener.class})
 @MappedSuperclass
 public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+        if (this.updatedAt == null) {
+            this.updatedAt = LocalDateTime.now();
+        }
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
